6e3b3301b389831b10c5631b63a3345edca2b873
breechoracion <breech@rmcordoviz.com>
2020-11-10 20:05:12 +0800

    fix: fixed client not updating

--- validate.php
+++ validate.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'carrental';
+$DATABASE_NAME = 'lyca-carrental';
  $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
  if ( mysqli_connect_errno() ) 
  {
